397b5764ca442ec449f77d4712f253db78462b01
Matheus Monteiro <matheus.stack.20@hotmail.com>
2023-05-31 23:06:11 -0300

    style: ajuste na navegação e na tela de apresentação

--- lib/src/chatbot.dart
+++ lib/src/chatbot.dart
@@ -1,4 +1,6 @@
 import 'package:chatbot/src/screen/chat/chat_screen.dart';
+import 'package:chatbot/src/screen/initial/initial_screen.dart';
+import 'package:chatbot/src/screen/tutorial/tutorial_screen.dart';
 import 'package:flutter/material.dart';
 
 class ChatBot extends StatelessWidget {
@@ -12,13 +14,12 @@ class ChatBot extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      // initialRoute: '/chat',
-      home: const ChatScreen(),
-      // routes: {
-      //   '/': (context) => const InitialScreen(),
-      //   '/tutorial': (context) => const TutorialScreen(),
-      //   '/chat': (context) => const ChatScreen(),
-      // },
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const InitialScreen(),
+        '/tutorial': (context) => const TutorialScreen(),
+        '/chat': (context) => const ChatScreen(),
+      },
     );
   }
 }
--- lib/src/screen/tutorial/tutorial_screen.dart
+++ lib/src/screen/tutorial/tutorial_screen.dart
@@ -9,34 +9,71 @@ class TutorialScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text('You AI Assistant'),
+            Container(
+              margin: const EdgeInsets.only(
+                top: 30,
+                bottom: 20,
+              ),
+              child: const Text(
+                'You AI Assistant',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 23,
+                ),
+              ),
+            ),
             const SizedBox(
               width: 250,
               child: Text(
                 'Using this software, you can ask you questions and receive articles using artificial intelligence assistant',
                 textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFF757575),
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
-            Container(
-              margin: const EdgeInsets.only(top: 20),
+            Expanded(
               child: Image.asset(
                 AppImagens.robotMast,
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/chat',
-                  (route) => false,
-                );
-              },
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Continue'),
-                  Icon(Icons.arrow_forward_outlined),
-                ],
+            Container(
+              width: 330,
+              height: 56,
+              margin: const EdgeInsets.only(bottom: 20),
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/chat',
+                    (route) => false,
+                  );
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Center(
+                      child: Text(
+                        'Continue',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 19,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
@@ -45,3 +82,10 @@ class TutorialScreen extends StatelessWidget {
     );
   }
 }
+
+/**
+ *   // Navigator.of(context).pushNamedAndRemoveUntil(
+                  //   '/chat',
+                  //   (route) => false,
+                  // );
+ */
